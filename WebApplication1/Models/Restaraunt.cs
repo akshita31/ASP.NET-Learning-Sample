41291938e0799e49cfd513a2bff28d42e389a97a
Akshita Agarwal <akagarw@microsoft.com>
2018-11-28 18:08:43 -0800

    Add label for the Create view

--- WebApplication1/Models/Restaraunt.cs
+++ WebApplication1/Models/Restaraunt.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SampleApplication.Models
 {
     public class Restaraunt
     {
         public int Id { get; set; }
+        [Display(Name = "Restaurant Name")]
+        [Required]
         public string Name { get; set; }
         public CuisineType Cuisine{get;set;}
     }
